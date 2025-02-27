1d2ecb4ce838cbfd154d1805ba8684f132b71d9b
joralvmel <joralvmel@gmail.com>
2025-02-27 12:57:52 +0100

    chore: update test packages and configs (#14)

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,7 +1,15 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  testMatch: ['**/tests/**/*.test.ts'],
+  clearMocks: true,
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
+  moduleNameMapper: {
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@application/(.*)$': '<rootDir>/src/application/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '^@interfaces/(.*)$': '<rootDir>/src/interfaces/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
   },
 };
