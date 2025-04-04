618b2a9174e3f3d17488b3cc3e99946c24525153
joralvmel <joralvmel@gmail.com>
2025-04-04 11:22:52 +0200

    feat: update recipeSearch to save more details (#40)

--- backend/src/infrastructure/repositories/recipeSearchSchema.ts
+++ backend/src/infrastructure/repositories/recipeSearchSchema.ts
@@ -6,6 +6,11 @@ const RecipeSearchSchema = new Schema<IRecipeSearch>(
     id: { type: Number, required: true, unique: true },
     title: { type: String, required: true },
     image: { type: String, required: false },
+    readyInMinutes: { type: Number, required: false },
+    healthScore: { type: Number, required: false },
+    cuisines: { type: [String], required: false },
+    dishTypes: { type: [String], required: false },
+    diets: { type: [String], required: false },
   },
   {
     versionKey: false,
--- backend/src/shared/mappers/RecipeMapper.ts
+++ backend/src/shared/mappers/RecipeMapper.ts
@@ -1,9 +1,30 @@
 import type { Recipe } from '@application/interfaces/recipeInterfaces';
 
-export function toRecipeDTO(recipe: { id: number; title: string; image: string }): Recipe {
+const capitalize = (str: string) => {
+  return str
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+};
+
+export function toRecipeDTO(recipe: {
+  id: number;
+  title: string;
+  image: string;
+  readyInMinutes: number;
+  healthScore: number;
+  cuisines: string[];
+  dishTypes: string[];
+  diets: string[];
+}): Recipe {
   return {
     id: recipe.id,
-    title: recipe.title,
+    title: capitalize(recipe.title),
     image: recipe.image,
+    readyInMinutes: recipe.readyInMinutes,
+    healthScore: recipe.healthScore,
+    cuisines: recipe.cuisines.map(capitalize),
+    dishTypes: recipe.dishTypes.map(capitalize),
+    diets: recipe.diets.map(capitalize),
   };
 }
