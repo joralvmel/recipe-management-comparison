ffa421b5a1ead50f6f4ace2fbcb0821a8948cbf3
joralvmel <joralvmel@gmail.com>
2025-04-10 14:31:39 +0200

    feat: add backend implementation for recipe detail (#19)

--- frontend-react/src/hooks/useRecipeDetail.ts
+++ frontend-react/src/hooks/useRecipeDetail.ts
@@ -1,20 +1,24 @@
 import type { RecipeType } from '../types';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { recipeData } from '../data/recipeData';
+import { fetchRecipeDetail } from '../services/recipeDetailService';
 
 const useRecipeDetail = () => {
   const { id } = useParams<{ id: string }>();
-  const [recipe] = useState<RecipeType | undefined>(() =>
-    recipeData.find((recipe) => recipe.externalId?.toString() === id)
-  );
-  const [servings, setServings] = useState(recipe?.servings || 1);
+  const [recipe, setRecipe] = useState<RecipeType | null>(null);
+  const [servings, setServings] = useState(1);
 
   useEffect(() => {
-    if (recipe) {
-      setServings(recipe.servings || 1);
-    }
-  }, [recipe]);
+    const getRecipeDetail = async () => {
+      if (id) {
+        const fetchedRecipe = await fetchRecipeDetail(id);
+        setRecipe(fetchedRecipe);
+        setServings(fetchedRecipe?.servings || 1);
+      }
+    };
+
+    getRecipeDetail();
+  }, [id]);
 
   const handleServingsChange = (newServings: number) => {
     setServings(newServings);
