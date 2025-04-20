3e230c3bd15772224392ef202300efdcef27e00d
joralvmel <joralvmel@gmail.com>
2025-04-20 15:00:28 +0200

    chore: update config (#20)

--- frontend-react/vite.config.ts
+++ frontend-react/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
       '@components': path.resolve(__dirname, './src/components'),
       '@pages': path.resolve(__dirname, './src/pages'),
       '@context': path.resolve(__dirname, './src/context'),
+      '@src': path.resolve(__dirname, './src'),
     },
   },
   test: {
