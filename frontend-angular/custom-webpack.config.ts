9877ef1ac2cade96466f6c5510e30216acda7905
joralvmel <joralvmel@gmail.com>
2025-05-08 15:51:24 +0200

    chore: update test environment (#30)

--- frontend-angular/custom-webpack.config.ts
+++ frontend-angular/custom-webpack.config.ts
@@ -1,5 +1,12 @@
-import { EnvironmentPlugin } from 'webpack';
-import Dotenv from 'dotenv-webpack';
-module.exports = {
-  plugins: [new Dotenv()],
-};
+import * as webpack from 'webpack';
+import DotenvWebpackPlugin from 'dotenv-webpack';
+
+export default {
+  plugins: [
+    new DotenvWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.USE_BACKEND': JSON.stringify(process.env['USE_BACKEND'] || 'false'),
+      'process.env.API_URL': JSON.stringify(process.env['API_URL'] || 'http://localhost:3000')
+    })
+  ]
+} as webpack.Configuration;
