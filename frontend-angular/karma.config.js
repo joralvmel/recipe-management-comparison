953d5641b2e76e5558aa8ea7af0ab8f26d4c09c8
joralvmel <joralvmel@gmail.com>
2025-05-20 00:41:15 +0200

    chore: add sonar config (#29)

---
+++ frontend-angular/karma.config.js
@@ -0,0 +1,28 @@
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'lcov' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['progress', 'kjhtml', 'coverage'],
+    browsers: ['ChromeHeadless'],
+    singleRun: true
+  });
+};
